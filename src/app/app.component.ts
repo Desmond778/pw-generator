b83eea5e6d8cef577b34175939862ef3d39d321d
Desmond Fernando <desmond.vinod@gmail.com>
2020-04-28 23:31:59 +0530

    Initial commit for PW Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-generator';
+
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let gerneratedPassword = '';
+
+    for(let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      gerneratedPassword += validChars[index];
+    }
+
+    this.password = gerneratedPassword;
+
+  }
+  
+  // onButtonClick() {
+  //   // console.log('Button was clicked');
+  //   console.log(`
+  //     About to generate a password with the following:
+  //     Include Letters : ${this.includeLetters}
+  //     Include Numbers : ${this.includeNumbers}
+  //     Include Symbols : ${this.includeSymbols}
+  //   `);
+  //   this.password = 'MY PASSWORD!!!';
+  // }
+
+  getName() {
+    return 'Desmond';
+  }
+  
 }
